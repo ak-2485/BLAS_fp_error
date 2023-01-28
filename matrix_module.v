660ed059b791efb1ed809318063de910304f871f
Ariel Kellison <ak2485@cornell.edu>
2023-01-28 13:02:12 -0500

    complete normed module for Banach fp theorem

--- matrix_module.v
+++ matrix_module.v
@@ -1,17 +1,29 @@
 
-From HB Require Import structures.
-From mathcomp Require Import all_ssreflect bigop all_algebra.
+(*From mathcomp Require Import all_ssreflect bigop all_algebra.
 From mathcomp Require Import ssralg ssrnum ssrint ssrnum finmap matrix mxalgebra.
 From mathcomp Require Import rat interval zmodp vector fieldext falgebra.
 From mathcomp Require Import cardinality set_interval mathcomp_extra.
-From mathcomp.analysis Require Import ereal signed reals normedtype Rstruct.
+From mathcomp.analysis Require Import  reals normedtype Rstruct.
+From mathcomp.analysis Require Import sequences functions set_interval mathcomp_extra.
+From mathcomp.analysis Require Import ereal signed topology normedtype prodnormedzmodule.
+From mathcomp.analysis Require Import classical_sets.*)
+From mathcomp Require Import all_ssreflect ssralg ssrint ssrnum finmap matrix.
+From mathcomp Require Import rat interval zmodp vector fieldext falgebra.
+From mathcomp Require Import boolp classical_sets functions.
+From mathcomp Require Import cardinality set_interval mathcomp_extra.
+From mathcomp Require Import ereal reals signed topology prodnormedzmodule normedtype sequences.
+
+Import Order.TTheory GRing.Theory Num.Def Num.Theory.
+Import numFieldTopology.Exports.
+Import numFieldNormedType.Exports.
+
+
 
 Import Order.Theory GRing.Theory Num.Def Num.Theory.
 
 Set Implicit Arguments.
 
 Local Open Scope ring_scope.
-
 Section Examples.
 Variables (R: realType) (m n : nat).
 
@@ -29,23 +41,7 @@ End Examples.
 
 Section Inverse.
 
-Variables (R: realType) (K: unitRingType) (m n : nat).
-
-Lemma ring_check (a1 a2 b : K):
-        a1 = a2 -> a1 * b = a2 * b. 
-Proof.
-by move => H; rewrite H.
-Qed.
-
-Lemma ring_check2 : forall (a1 a2 b : K), 
-        b \is a GRing.unit -> a1 * b = a2 * b -> a1 = a2. 
-Proof.
-move => a1 a2 b H1 H2. 
-pose proof ring_check as H0.
-specialize (H0 (a1 * b) (a2 * b) (b^-1) H2).
-rewrite -!mulrA mulrV in H0 => [ | //].
-by rewrite !mulr1 in H0.
-Qed.
+Variables (R: realType) (m n : nat).
 
 Lemma eqmx_mulr (b a1 a2: 'M[R]_m.+1) :
         a1 = a2 -> a1 * b = a2 * b. 
@@ -73,7 +69,6 @@ have H4: invmx (A *m B) * (A *m B) = invmx B *m invmx A * (A *m B).
 apply (mulr_eqmx _ _ _ H3 H4).
 Qed.
 
-
 Lemma invmx_pert (A E: 'M[R]_m.+1) :
         A \in unitmx -> 1 + invmx A * E \in unitmx ->
         invmx (A + E) = invmx (1 + invmx A * E) * invmx A.
@@ -83,5 +78,94 @@ have H3: invmx (A + E) = invmx (A * (1 + invmx A * E)) by
 rewrite mulrDr mulrA mulr1 -mulmxE (mulmxV H1) mul1mx //.
 rewrite H3 (invmx_mul _ _ H1) //.
 Qed.
+
+End Inverse.
+
+Section Maps.
+Variables (m n : nat).
+Variables (R: realType) (T: topologicalType).
+Variable  (M: 'M[T]_(m.+1,m.+1)).
+
+(* contractive maps are defd in sequences file*)
+Definition f_app (f : 'rV[R]_m -> 'rV[R]_m)
+        (u: 'rV[R]_m)  := fun n => iter n f u.
+
+Goal forall (f : 'rV[R]_m -> 'rV[R]_m) (u: 'rV[R]_m) ,
+       f_app f u 1 = f u.
+rewrite/f_app/iter //. Qed.
+
+Goal forall (f : 'rV[R]_m -> 'rV[R]_m) (u: 'rV[R]_m) ,
+       f_app f u 0 =  u.
+rewrite/f_app/iter //. Qed.
+
+
+
+Variable  (R0 : realFieldType) (AM: set (matrix R m.+1 1)) .
+Variable (AM1: set 'M[R]_(m.+1)) .
+Variable (AV : set 'cV[R]_(m.+1)).
+
+Canonical mat_R_CompleteNormedModule (a b : nat) :=
+  [completeNormedModType R of (matrix R a.+1 b.+1)].
+
+Import topology.numFieldTopology.Exports.
+Import CompleteNormedModule.Exports.
+
+From elpi Require Import elpi.
+
+From HB Require Import structures.
+
+
+(* A1 and AR work for statement, AV does not*)
+Theorem Ban : forall (f : {fun AV >-> AV}) ,
+is_contraction f -> closed AV ->
+        exists2 x, AV x & x = f x .
+Proof.
+intros.
+ apply banach_fixed_point.
+Admitted.
+
+Locate closed.
+Print closed.
+
+Print bounded_closed_compact.
+
+Lemma bounded_closed_compact (R0 : realType) p (A1: set 'rV[R0]_p.+1) :
+  bounded_set A1 -> closed A1 -> compact A1.
+
+
+Theorem Ban
+(R1 : realType) n (A1 : set 'rV[R1]_n.+1)
+ : forall (f : {fun A1 >-> A1}) ,
+is_contraction f -> closed A1 ->
+        exists2 x, A x & x = f x .
+Proof.
+
+Variable (K: numDomainType).
+Variable U : set 'rV[T]_m.
+Variable f : {fun A >-> A}.
+
+Goal f U = f U.
+
+Goal is_contraction f.
+
+
+
+Search nbhs matrix.
+Definition mat_nbhs := nbhs M.
+Print nbhs.
+
+
+(* definition of the matrix condition number *)
+Definition kappa (A : 'M[R]_m.+1) := `|(invmx A)| * `|A|.
+
+Lemma Neumann_bound' (A : 'M[R]_m.+1):
+        (1 - A) * \sum_(0 <= i < n.+1) A^i.
+Proof.
+
+Lemma Neumann_bound (F : 'M[R]_m.+1):
+        `|invmx(1 - F)| <=  \sum_(0 <= i < n.+1) `|F|^i.
+Proof.
+unfold norm.
+
 
 
